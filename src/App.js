93e4d64fb1e491af39b6eafcc7ad25a770a99c31
gradity <freshairnugget@gmail.com>
2022-02-07 22:52:40 +0700

    Meals component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './index.css';
 
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 export default function App() {
   return (
     <>
       <Header />
-      <h1>Hello StackBlitz!!</h1>
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
